16ec7944c3b1a4242b1bbfcf49b9fd8de3ae50e3
jgonzalez-2024374 <jgonzalez-2024374@kinal.edu.gt>
2026-04-27 10:15:24 -0600

    fix: modified route on auth-service

--- authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0859b86fe73b10324d09fcc9ca2feb632c8df96c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0353233917d6cc12924249aa45f81c121d7177b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
